804fb4dfbd67b638d22a3f329aa7cf80962498c9
Clinton Hadinata <csghadinata@gmail.com>
2016-10-26 10:58:18 +1100

    Handled extracting values from each line in a config file

--- lsr.py
+++ lsr.py
@@ -6,7 +6,6 @@
 
 import sys
 
-
 # input arguments
 node_id = sys.argv[1]
 node_port = int(sys.argv[2])
@@ -15,8 +14,7 @@ config_filename = sys.argv[3];
 # open file and break it up into segments
 f = open(config_filename)
 filestring = f.read()
-config_lines = filestring.split("\n");
-# segments = [filestring[i:i+mss] for i in range(0, len(filestring), mss)]
+config_lines = filestring.split("\n")
 f.close()
 
 print node_id
@@ -27,5 +25,18 @@ num_lines = config_lines[0]
 
 print num_lines
 
+# hash storing the cost to travel to a node
+cost = {}
+
+# hash storing the port number of a node
+port_number = {}
+
 for i in range(1,int(num_lines)+1):
-    print config_lines[i]
+    line = config_lines[i]
+    line_elements = line.split(" ")
+    node = line_elements[0]
+    cost[node] = line_elements[1]
+    port_number[node] = line_elements[2]
+    print node
+    print cost[node]
+    print port_number[node]
