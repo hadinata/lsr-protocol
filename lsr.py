cee0b2518ed6cab98176d7b434e906cd0dff3bb9
Clinton Hadinata <csghadinata@gmail.com>
2016-10-26 15:28:15 +1100

    Defined flood function to send all neighbouring nodes link-state packets created with newly defined createLinkStateHeader function

--- lsr.py
+++ lsr.py
@@ -4,7 +4,13 @@
 # COMP3331 2016 S2 Assignment 2
 # By Clinton Hadinata, October 2016
 
+import socket
 import sys
+import threading
+import time
+
+# constants
+FLOODING_INTERVAL = 1.0
 
 # input arguments
 node_id = sys.argv[1]
@@ -21,9 +27,12 @@ print node_id
 print node_port
 print config_lines
 
-num_lines = config_lines[0]
+num_neighbours = config_lines[0]
 
-print num_lines
+print num_neighbours
+
+# list containing all the current node's neighbouring nodes
+neighbours = []
 
 # hash storing the cost to travel to a node
 cost = {}
@@ -31,12 +40,41 @@ cost = {}
 # hash storing the port number of a node
 port_number = {}
 
-for i in range(1,int(num_lines)+1):
+for i in range(1,int(num_neighbours)+1):
     line = config_lines[i]
     line_elements = line.split(" ")
     node = line_elements[0]
+    neighbours.append(node)
     cost[node] = line_elements[1]
     port_number[node] = line_elements[2]
     print node
     print cost[node]
     print port_number[node]
+
+# create default header based on current state
+def createLinkStateHeader():
+    h_num_neighbours = str(num_neighbours) #str(num_neighbours).zfill(NUM_NEIGHBOURS_BYTES)
+    h_info = ""
+    for i in range(0,len(neighbours)):
+        h_info += str(" ") + str(cost[neighbours[i]]) + str(" ") + str(port_number[neighbours[i]])
+    header = h_num_neighbours + h_info
+    return header
+
+def flood():
+    # threading to repeat every flooding interval
+    # code adopted from http://stackoverflow.com/questions/3393612/run-certain-code-every-n-seconds
+    threading.Timer(FLOODING_INTERVAL, flood).start()
+
+    # Declare socket
+    udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # message to send over socket
+    message = createLinkStateHeader()
+
+    # send message to all neighbour nodes
+    for i in range(0,len(neighbours)-1):
+        neighbour_node = neighbours[i]
+        udpSocket.sendto(message,("localhost", int(port_number[neighbour_node])))
+        # print "SENT ===>     [ " + message + " ] to " + neighbour_node;
+
+flood()
