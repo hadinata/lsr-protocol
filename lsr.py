74dc6f2656a75161cc1021ea264b9fba05b37333
Clinton Hadinata <csghadinata@gmail.com>
2016-10-28 20:04:43 +1100

    Cleaned up code, added comments

--- lsr.py
+++ lsr.py
@@ -9,8 +9,10 @@ import threading
 import time
 
 # constants
-UPDATE_INTERVAL = 1.0     # one second
-ROUTE_UPDATE_INTERVAL = 30.0
+UPDATE_INTERVAL = 1.0           # one second
+ROUTE_UPDATE_INTERVAL = 30.0    # 30 seconds
+HEARTBEAT_BROADCAST_INTERVAL = 1.0
+HEARTBEAT_CHECK_INTERVAL = 1.0
 
 # input arguments
 node_id = sys.argv[1]
@@ -18,14 +20,8 @@ node_port = int(sys.argv[2])
 config_filename = sys.argv[3];
 
 # global network topology graph represented as a 2d dict:
-# graph[node][connecting node] storing the cost between node and connecting node
+# graph[node][connecting-node] stores the cost between node and connecting-node
 graph = {}
-
-# open config file and break it up line by line
-f = open(config_filename)
-filestring = f.read()
-config_lines = filestring.split("\n")
-f.close()
 
 # list containing all the current node's neighbouring nodes
 neighbours = []
@@ -35,6 +31,12 @@ cost = {}
 
 # hash storing the port number of a node
 port_number = {}
+
+# open config file and break it up line by line
+f = open(config_filename)
+filestring = f.read()
+config_lines = filestring.split("\n")
+f.close()
 
 # number of neighbour nodes is the first line of the config file
 num_neighbours = config_lines[0]
@@ -48,21 +50,20 @@ for i in range(1,int(num_neighbours)+1):
     cost[node] = float(line_elements[1])
     port_number[node] = line_elements[2]
 
-#print neighbours
-
-# heartbeat variables
+# heartbeat global variables
 hbBroadcasts = 0
 hbCount = {}
 
+# set of deleted nodes
 deleted_nodes = set()
 
 # Declare socket
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udpSocket.bind(('', node_port))
 
+
 # create link state packet based on current state
 def createLSP():
-    #h_num_neighbours = str(num_neighbours) #str(num_neighbours).zfill(NUM_NEIGHBOURS_BYTES)
     h_node_id = str(node_id)
     h_info = ""
     for i in range(0,len(neighbours)):
@@ -70,45 +71,42 @@ def createLSP():
     h_deleted_nodes = ""
     for del_node in deleted_nodes:
         h_deleted_nodes += "," + del_node
-    header = h_node_id + h_info + "|" + h_deleted_nodes
-    return header
+    packet = h_node_id + h_info + "|" + h_deleted_nodes
+    return packet
 
 # updates network graph based on received link state packet
 def updateGraph(message):
     two_parts = message.split("|")
-    left_nodes = two_parts[0]
-    right_deleted = two_parts[1]
+    left_nodes = two_parts[0]           # left part of packet
+    right_deleted = two_parts[1]        # right part of packet
 
     nodes = left_nodes.split(",")
-    from_node = nodes[0]
+    from_node = nodes[0]                # first element is the origin node
     for i in range(1, len(nodes)):
         to_node_props = nodes[i].split(" ")
-        tn_id = to_node_props[0]            # connecting node id
-        tn_cost = float(to_node_props[1])     # connecting node cost
-        tn_port = int(to_node_props[2])     # connecting node port
+        tn_id = to_node_props[0]                # connecting-node id
+        tn_cost = float(to_node_props[1])       # connecting-node cost
+        tn_port = int(to_node_props[2])         # connecting-node port
 
-        if  graph.has_key(from_node):        # if from_node already defined as a key
-            graph[from_node][tn_id] = tn_cost        # update key value
+        if  graph.has_key(from_node):           # if from_node already defined as a key
+            graph[from_node][tn_id] = tn_cost   # update key value
         else:
             graph[from_node] = { tn_id: tn_cost }    # initialise key value
 
-        if graph.has_key(tn_id):
+        if graph.has_key(tn_id):                # repeat for the reverse
             graph[tn_id][from_node] = tn_cost
         else:
             graph[tn_id] = { from_node: tn_cost }
 
-    del_nodes = right_deleted.split(",")
+    del_nodes = right_deleted.split(",")        # remove all deleted nodes from topology
     for i in range(1, len(del_nodes)):
         removeNode(del_nodes[i])
 
-        #print graph
-
-
 # floods neighbours with broadcast of current neighbour information
-def flood():
-    # threading to repeat flooding every interval
+def broadcastLSP():
+    # threading to repeat broadcast every interval
     # code adopted from http://stackoverflow.com/questions/3393612/run-certain-code-every-n-seconds
-    threading.Timer(UPDATE_INTERVAL, flood).start()
+    threading.Timer(UPDATE_INTERVAL, broadcastLSP).start()
 
     # message to send over socket
     message = createLSP()
@@ -117,65 +115,61 @@ def flood():
     for i in range(0,len(neighbours)):
         neighbour_node = neighbours[i]
         udpSocket.sendto(message,('localhost', int(port_number[neighbour_node])))
-        # print "BROADCAST ===>     [" + message + "] to " + neighbour_node + " at port " + port_number[neighbour_node] + ""
 
-flood()
+broadcastLSP()
 
+# broadcast heartbeat message
 def broadcastHeartbeat():
-    threading.Timer(1, broadcastHeartbeat).start()
+    # threading to repeat every heartbeat broadcast interval
+    threading.Timer(HEARTBEAT_BROADCAST_INTERVAL, broadcastHeartbeat).start()
 
+    # packet message:
     message = "HB" + " " + node_id
 
+    # send heartbeat to all neighbours
     for i in range(0,len(neighbours)):
         neighbour_node = neighbours[i]
         udpSocket.sendto(message,('localhost', int(port_number[neighbour_node])))
 
+    # increment hbBroadcasts
     global hbBroadcasts
     hbBroadcasts += 1
 
 broadcastHeartbeat()
 
+# removes node from network topology graph
 def removeNode(node_to_remove):
-    # print "NEIGHBOURS"
-    # print neighbours
+
+    # add node to deleted_nodes set
     deleted_nodes.add(node_to_remove)
-    #num_neighbours -= 1
+
+    # remove first-dimension keys of the node from topology graph
     try:
-        # print graph
         del graph[node_to_remove]
-        # print "DELETED " + node_to_remove
-        # print graph
     except:
         return
 
+    # remove second-dimension keys of the node
     for node in graph.keys():
         if graph[node].has_key(node_to_remove):
             del graph[node][node_to_remove]
 
+# checks to see if any nodes have failed
 def checkHeartbeat():
-    threading.Timer(2, checkHeartbeat).start()
-    # print "CHECKING"
-    # print hbBroadcasts
-    # print hbCount.keys()
+    # threading to repeat every heartbeat check interval
+    threading.Timer(HEARTBEAT_CHECK_INTERVAL, checkHeartbeat).start()
+
+    # check each node and see if hbBroadcasts-hbCount[node] > 3
     for node in hbCount.keys():
-        # print node + " " + str(hbCount[node])
         if hbBroadcasts-hbCount[node] > 3:
-            # print "REMOVING " + node
             neighbours.remove(node)
             removeNode(node)
             del hbCount[node]
-            # print "HB COUNT: "
-            # print hbCount
 
 checkHeartbeat()
 
-def printprint(D, N_dash, prev):
-    print "\n"
-    print N_dash
-    for d in D:
-        print "d: " + d + " cost: " + str(D[d]) + " prev: " + prev[d]
-    print "\n"
-
+# helper function for Dijkstra algorithm
+# finds the minimum cost node to travel to
 def minimumCost(D, N_dash):
     curr_min = ""
     for node in graph.keys():
@@ -188,88 +182,70 @@ def minimumCost(D, N_dash):
                 curr_min = node
     return curr_min
 
-
+# dijkstra algorithm to find shortest path to each node in topology
 def dijkstra():
     # threading to repeat every route update interval
     # code adopted from http://stackoverflow.com/questions/3393612/run-certain-code-every-n-seconds
     threading.Timer(ROUTE_UPDATE_INTERVAL, dijkstra).start()
 
     # Declare variables
-    N_dash = set()
-    D = {}
-    prev = {}
-    u = node_id
+    N_dash = set()      # set of nodes we know the minimum path to
+    D = {}              # hash of current minimum cost to travel to key node
+    prev = {}           # hash of the previous node for minimum cost travel to key node
+    u = node_id         # u is the origin id
 
     # Initialisation
     N_dash.add(node_id)
-    for v in graph.keys():
+    for v in graph.keys():      # for each node in the topology graph
+        # if there is a connection between this node and v, set D[v] to the cost
         if (graph[u].has_key(v) and graph[u][v] > 0):
             cost = graph[u][v]
             D[v] = cost
             prev[v] = u
-        else:
+        else:               # else, D[v] is infinite
             D[v] = float("inf")     # simulating infinity
             prev[v] = ""
 
-    # print "After initialisation: "
-    # printprint(D, N_dash, prev)
-
-    # Loop
-    # print len(N_dash)
-    # print len(graph.keys())
+    # while we still haven't found minimum path to all the nodes:
     while len(N_dash) < len(graph.keys()):
-        #w = min(D, key=D.get)
-        # print "D IS: "
-        # print D
-        w = minimumCost(D, N_dash)
-        # print "MINIMUM IS: " + w
-        N_dash.add(w)
-        # printprint(D, N_dash, prev)
-        # update D(v) for each neighbor v of w and not in N
-        for v in graph[w].keys():
+        w = minimumCost(D, N_dash)  # w is node with minimum D[]
+        N_dash.add(w)               # update N'
+        for v in graph[w].keys():   # update D[]
             if (v == node_id):
                 continue
-            # print "Updating D at " + v
             if (v not in N_dash):
                 cost = graph[w][v]
                 if (D[w] + cost < D[v]):
                     D[v] = D[w] + cost
                     prev[v] = w
-            # printprint(D, N_dash, prev)
-
-    # print "GRAPH KEYS  "
-    # print graph.keys()
-    # print "N_DASHHH  "
-    # print N_dash
 
     # print least cost route
     for node in graph.keys():
-        # print "\nAT NODE: " + node
-        # print "PREV: " + prev[node]
         if node == node_id:
             continue
         else:
             route = node
             total_cost = float(0)
+
             current = node
             nextback = prev[node]
             while nextback != node_id:
                 cost = graph[nextback][current]
                 total_cost += cost
                 route += nextback
-                # print "ROUTE: " + route
-                # print "TOTCOST: " + str(total_cost)
                 current = nextback
                 nextback = prev[nextback]
             cost = graph[node_id][current]
             total_cost += cost
             route += node_id
+
             route = ''.join(reversed(route))
             print "least-cost path to node " + node + ": " + route + " and the cost is " + str(total_cost)
     print "\n"
 
 dijkstra()
 
+# returns the origin node id of a given link-state packet
 def getNodeID(message):
     two_parts = message.split("|")
     left_nodes = two_parts[0]
@@ -279,6 +255,7 @@ def getNodeID(message):
     from_node = nodes[0]
     return from_node
 
+# returns the neighbours of the node of a given link-state packet
 def getNodeNeighbours(message):
     neighbours_to_return = []
 
@@ -295,25 +272,25 @@ def getNodeNeighbours(message):
 
     return neighbours_to_return
 
+
 # infinite loop, listens in to receive packets
 while 1:
     message, fromAddress = udpSocket.recvfrom(2048)
     fromIP, fromPort = fromAddress
-    #print "RECV ====> [" + message + "] from port " + str(fromPort)
     hb_match = re.match(r'^HB ([\w\d]+)$',message)
     hb_ack_match = re.match(r'^HB-ACK ([\w\d]+)$',message)
-    if hb_match:
+    if hb_match:                                # if a heartbeat packet
         from_node = hb_match.group(1)
-        reply = "HB-ACK " + node_id
+        reply = "HB-ACK " + node_id             # respond with HB-ACK
         udpSocket.sendto(reply, ('localhost', int(port_number[from_node])))
-    elif hb_ack_match:
+    elif hb_ack_match:                          # if an HB-ACK
         from_node = hb_ack_match.group(1)
-        if hbCount.has_key(from_node):
+        if hbCount.has_key(from_node):          # update hbCount
             hbCount[from_node] += 1
         else:
             hbCount[from_node] = hbBroadcasts
-    else:
-        updateGraph(message)
+    else:                                       # if not a HB or HB-ACK:
+        updateGraph(message)                    # update new topology
         # pass message on to all neighbour nodes
         for i in range(0,len(neighbours)):
             neighbour_node = neighbours[i]
@@ -322,4 +299,3 @@ while 1:
                 continue
             else:
                 udpSocket.sendto(message,('localhost', int(port_number[neighbour_node])))
-                #print "SENT ===>     [" + message + "] to " + neighbour_node + " at port " + port_number[neighbour_node] + ""
